d44505ce3c8047b377c7cb027b5f3da3ed4dfa08
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-03-27 04:47:53 +0000

    updates saveTweet to run callback once insert in database is sucessful and complete

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -7,8 +7,7 @@ module.exports = function makeDataHelpers(db) {
   return {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      db.collection('tweets').insertOne(newTweet);
-      callback(null, true);
+      db.collection('tweets').insertOne(newTweet, callback(null, true));
     },
 
     // Get all tweets in `db`, sorted by newest first
